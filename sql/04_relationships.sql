c8d981f76eab964c645111e00c3205885a22870d
CaddisMaster <135708275+CaddisMaster@users.noreply.github.com>
2026-05-13 22:54:53 -0400

    add foreign key relationships between tables

---
+++ sql/04_relationships.sql
@@ -0,0 +1,12 @@
+ALTER TABLE transactions 
+ADD CONSTRAINT fk_transactions_category
+fOREIGN KEY (category_id)
+REFERENCES categories (id)
+on DELETE RESTRICT;
+
+
+ALTER TABLE budgets
+ADD CONSTRAINT fk_budgets_category
+FOREIGN KEY (category_id)
+REFERENCES categories (id)
+ON DELETE RESTRICT;

6356cf486abd2e6b5a38b8f73fd6d82ede805f1b
CaddisMaster <135708275+CaddisMaster@users.noreply.github.com>
2026-06-09 20:02:44 -0400

    updated schema Users table to have column name be username

--- sql/schema.sql
+++ sql/schema.sql
@@ -59,7 +59,7 @@ CREATE TABLE public.budgets (
 -- ------------------------------------------------------------
 CREATE TABLE public.users (
     id SERIAL PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     is_admin BOOLEAN NOT NULL DEFAULT false,
     created_at TIMESTAMP DEFAULT NOW()
